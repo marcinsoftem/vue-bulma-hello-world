73a570665bc1f48ba6d7261343db4fc5e8b5f7bb
Marcin Mikłas <m.miklas@wszib.edu.pl>
2024-09-14 07:13:22 +0200

    Zoptymalizowano zapytania REST i ustawiono focus

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,29 +1,61 @@
 <script setup>
-import { ref } from "vue";
+import { ref, useTemplateRef, onMounted, onUpdated } from "vue";
 let element = ref("");
 let list = ref(null);
+const elementInput = useTemplateRef("elementInput");
 
+onMounted(() => {
+  getElements();
+  setElementInputFocus();
+});
 
-fetch("http://localhost:8080/elements")
-    .then((response) => response.json())
-    .then((data) => {list.value = data});
+onUpdated(() => {
+  setElementInputFocus();
+});
 
+function setElementInputFocus() {
+  elementInput.value.focus();
+}
+
+function getElements() {
+  fetch("http://localhost:8080/elements")
+      .then((response) => response.json())
+      .then((data) => {
+        list.value = data
+        console.log("Getting elements: " + list.value);
+      })
+      .catch(function (error) {
+        console.log("Getting elements error: " + error);
+      });
+}
 
 function addElement() {
   if (element.value == "") return;
   fetch("http://localhost:8080/elements", {
     method: "POST",
     body: element.value,
+  }).then(function (response) {
+    if (response.ok) {
+      console.log("Adding element: " + element.value);
+      element.value = "";
+      getElements();
+    }
+  }).catch(function (error) {
+    console.log("Adding element error: " + error);
   });
-  list.value.push(element.value);
-  element.value = "";
 }
 
 function removeElement(index) {
   fetch("http://localhost:8080/elements/" + index,  {
     method: "DELETE",
+  }).then(function (response) {
+    if (response.ok) {
+      console.log("Deleting element: " + list.value[index]);
+      getElements();
+    }
+  }).catch(function (error) {
+    console.log("Deleting element error: " + error);
   });
-  list.value.splice(index, 1);
 }
 </script>
 
@@ -35,7 +67,7 @@ function removeElement(index) {
         <button class="delete" v-on:click="removeElement(index)"></button>
       </p>
       <div class="field is-grouped">
-        <input type="text" class="input" v-model="element" v-on:keyup.enter="addElement" placeholder="Wprowadź element" autofocus/>
+        <input type="text" class="input" v-model="element" v-on:keyup.enter="addElement" placeholder="Wprowadź element" ref="elementInput"/>
         <button class="button is-link" v-on:click="addElement">Dodaj</button>
       </div>
     </div>
